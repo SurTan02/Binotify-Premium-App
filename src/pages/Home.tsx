1f9edb8ff984fdb2bc47059ee0df5b7395888e9c
Suryanto Tan <suryanto.tan5@gmail.com>
2022-11-30 00:35:03 +0700

    feat: change state on add song

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,7 @@ function Home() {
 
   useEffect(() => {
     getUsers(currentPage);
-  }, [currentPage]);
+  }, [() => getUsers(currentPage)]);
 
   const callbackOpenModal = () => {
     setIsOpen(true);
